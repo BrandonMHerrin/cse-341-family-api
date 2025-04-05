6d87c1432e096d051b8959a4561348d938bfa413
BrandonMHerrin <brandon.herrin@live.com>
2025-04-04 20:50:25 -0600

    Redirect authentication callbacks to API documentation instead of home page

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -4,12 +4,12 @@
  * @param {Response} res 
  */
 export const handleGihubCallBack = (req,res) => {
-    res.redirect('/');
+    res.redirect('/api-docs');
 };
 
 export const handleLogout = (req, res) => {
     req.logout((error) => {
         if (error) return res.status(500).send(error.message);
-        res.redirect('/');
+        res.redirect('/api-docs');
     })
 }
